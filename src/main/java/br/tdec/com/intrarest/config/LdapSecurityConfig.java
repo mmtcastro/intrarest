941a3e8c05f797cf35c0c698f5d6dbe41c831664
mcast <mcast@ACER-NITRO-MCASTRO>
2023-05-24 23:11:06 -0300

    is it possible to get a UserDetailsService from LdapContextSource?

--- src/main/java/br/tdec/com/intrarest/config/LdapSecurityConfig.java
+++ src/main/java/br/tdec/com/intrarest/config/LdapSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.BaseLdapPathContextSource;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.security.ldap.authentication.BindAuthenticator;
+import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsMapper;
 
 @Configuration
 public class LdapSecurityConfig {
@@ -59,6 +63,13 @@ public class LdapSecurityConfig {
 		contextSource.setReferral("follow");
 		contextSource.afterPropertiesSet();
 
+		LdapAuthenticationProvider authenticationProvider = new LdapAuthenticationProvider(
+				new BindAuthenticator(contextSource), new DefaultLdapAuthoritiesPopulator(contextSource, "O=TDec"));
+		authenticationProvider.setUserDetailsContextMapper(new LdapUserDetailsMapper());
+
+		LdapAuthenticationProvider authenticationProvider = new LdapAuthenticationProvider(
+				new BindAuthenticator(contextSource));
+
 		System.out.println("--> Isto eh " + contextSource.getPassword());
 
 		System.out.println("contextSource -> " + contextSource.getContext("CN=Marcelo Castro, O=TDec", "Hodge$404"));
@@ -69,7 +80,6 @@ public class LdapSecurityConfig {
 	@Bean
 	public LdapTemplate ldapTemplate() {
 		LdapTemplate template = new LdapTemplate(contextSource());
-
 		return template;
 	}
 
--- src/main/java/br/tdec/com/intrarest/security/SecurityConfig.java
+++ src/main/java/br/tdec/com/intrarest/security/SecurityConfig.java
@@ -1,13 +1,12 @@
 package br.tdec.com.intrarest.security;
 
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
@@ -17,26 +16,37 @@ import br.tdec.com.intrarest.view.login.LoginView;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends VaadinWebSecurity {
-	private static class SimpleInMemoryUserDetailsManager extends InMemoryUserDetailsManager {
-		public SimpleInMemoryUserDetailsManager() {
-			createUser(
-					new User("user", "{noop}userpass", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))));
-			createUser(new User("mcastro", "{noop}Hodge$404",
-					Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-		}
-	}
+
+	// private static class SimpleInMemoryUserDetailsManager extends
+	// InMemoryUserDetailsManager {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests().requestMatchers("/images/**").permitAll();
-
 		super.configure(http);
-
 		setLoginView(http, LoginView.class);
 	}
 
 	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		return new SimpleInMemoryUserDetailsManager();
+	public UserDetailsService users() {
+		UserDetails user = User.builder().username("user")
+				// password = password with this hash, don't tell anybody :-)
+				.password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW").roles("USER").build();
+		UserDetails admin = User.builder().username("Marcelo Castro")
+				.password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW").roles("USER", "ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(user, admin);
 	}
+
+//		public SimpleInMemoryUserDetailsManager() {
+//			createUser(
+//					new User("user", "{noop}userpass", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))));
+//			createUser(new User("mcastro", "{noop}Hodge$404",
+//					Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+//		}
+
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsService() {
+//		return new SimpleInMemoryUserDetailsManager();
+//	}
 }
